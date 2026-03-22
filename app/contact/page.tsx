17580338ae57ddf67b07a810c28fa1fc982b03a0
jessiiishin <jessiiishin@gmail.com>
2026-03-22 01:52:06 -0400

    edits and font stuff to contact page web

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,36 +8,36 @@ export default function Contacts() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-8">
-      <h1 className="text-4xl font-sourcesans font-bold text-primary">Contact</h1>
+      <h1 className="text-6xl font-bold text-primary mb-[60px]">Contact</h1>
       <form className="flex flex-col gap-8 w-full max-w-4xl" onSubmit={handleSubmit}>
         <div className="flex gap-8">
           <div className="flex flex-col gap-4 flex-1">
             <input
               type="text"
-              placeholder="Name"
-              className="px-6 py-3 border border-solid border-[1px] border-brown rounded-full placeholder-gray focus:outline-none"
+              placeholder="Name*"
+              className="px-6 py-3 text-2xl border border-solid border-[1px] border-brown rounded-full placeholder-gray focus:outline-none"
             />
             <input
               type="email"
-              placeholder="Email"
-              className="px-6 py-3 border border-solid border-[1px] border-brown rounded-full placeholder-gray focus:outline-none"
+              placeholder="Email*"
+              className="px-6 py-3 text-2xl border border-solid border-[1px] border-brown rounded-full placeholder-gray focus:outline-none"
             />
             <input
               type="tel"
               placeholder="Phone"
-              className="px-6 py-3 border border-solid border-[1px] border-brown rounded-full placeholder-gray focus:outline-none"
+              className="px-6 py-3 text-2xl border border-solid border-[1px] border-brown rounded-full placeholder-gray focus:outline-none"
             />
           </div>
           
           <div className="flex flex-col flex-1">
             <textarea
               placeholder="Message"
-              className="px-6 py-3 border border-solid border-[1px] border-brown rounded-[50px] placeholder-gray focus:outline-none flex-1 resize-none"
+              className="p-5 text-2xl border border-solid border-[1px] border-brown rounded-[50px] placeholder-gray focus:outline-none flex-1 resize-none"
             />
           </div>
         </div>
 
-        <button type="submit" className="px-[40px] py-[10px] bg-primary text-white rounded-full hover:bg-blue-700 self-center">
+        <button type="submit" className="px-[40px] py-[20px] text-2xl leading-[28px] bg-primary text-white rounded-full box-border border-2 border-transparent hover:border-brown hover:bg-transparent hover:text-brown active:border-transparent active:bg-softgray active:text-gray self-end">
           Send Message
         </button>
       </form>
